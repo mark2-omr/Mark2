fe47f2026d90e097726154f55b141e7f3c450b18
green <green.grosjean@gmail.com>
2024-01-09 23:10:54 +0900

    Fix IDE0290
    https://learn.microsoft.com/dotnet/fundamentals/code-analysis/style-rules/ide0290

--- Models/Square.cs
+++ Models/Square.cs
@@ -8,14 +8,4 @@ public class Square
     public int H;
     public int Cx;
     public int Cy;
-
-    public Square(int x, int y, int w, int h, int cx, int cy)
-    {
-        X = x;
-        Y = y;
-        W = w;
-        H = h;
-        Cx = cx;
-        Cy = cy;
-    }
 }
