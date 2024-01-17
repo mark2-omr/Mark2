13ff73d018de98ec56fbc477f77a85d4d9f81038
Kaoru Mori <kaoru.mori@defrag.works>
2024-01-17 22:24:48 +0900

    Change member variable names for Area and Square

--- Models/Area.cs
+++ Models/Area.cs
@@ -4,7 +4,7 @@ public class Area
 {
     public int X { get; set; }
     public int Y { get; set; }
-    public int W { get; set; }
-    public int H { get; set; }
-    public int V { get; set; }
+    public int Width { get; set; }
+    public int Height { get; set; }
+    public int Value { get; set; }
 }
--- Models/Square.cs
+++ Models/Square.cs
@@ -4,8 +4,8 @@ public class Square
 {
     public int X { get; set; }
     public int Y { get; set; }
-    public int W { get; set; }
-    public int H { get; set; }
-    public int Cx { get; set; }
-    public int Cy { get; set; }
+    public int Width { get; set; }
+    public int Height { get; set; }
+    public int CenterX { get; set; }
+    public int CenterY { get; set; }
 }
